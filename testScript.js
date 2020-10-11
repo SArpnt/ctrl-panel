0152a2267df0e815a49f396c5767724951ace051
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-10-11 12:50:46 -0400

    Create testScript.js

---
+++ testScript.js
@@ -0,0 +1,32 @@
+btnAPI.addButton('left');
+btnAPI.addButton('top', 'xxxxxl', 'info', 'xxxxxl');
+btnAPI.addButton('top', 'xxxxl', 'info', 'xxxxl');
+btnAPI.addButton('top', 'xxxl', 'danger', 'xxxl');
+btnAPI.addButton('top', 'xxl', 'danger', 'xxl');
+btnAPI.addButton('top', 'xl', 'warning', 'xl');
+btnAPI.addButton('top', 'lg', 'secondary', 'lg');
+btnAPI.addButton('top', 'md', 'dark', 'md');
+btnAPI.addButton('top', 'sm', 'secondary', 'sm');
+btnAPI.addButton('top', 'xs', 'warning', 'xs');
+btnAPI.addButton('top', 'xxs', 'danger', 'xxs');
+btnAPI.addButton('top', 'xxxs', 'danger', 'xxxs');
+btnAPI.addButton('top', 'xxxxs', 'info', 'xxxxs');
+btnAPI.addButton('top', 'xxxxxs', 'info', 'xxxxxs');
+btnAPI.addButton('top');
+btnAPI.addButton('top', 'lllllllllllllllloooooooooooooooonnnnnnnnnnnnnnnngggggggggggggggg');
+btnAPI.addButtonGroup('bottom',
+	['xxxxxl', 'info', 'xxxxxl'],
+	['xxxxl', 'info', 'xxxxl'],
+	['xxxl', 'danger', 'xxxl'],
+	['xxl', 'danger', 'xxl'],
+	['xl', 'warning', 'xl'],
+	['lg', 'secondary', 'lg'],
+	['md', 'dark', 'md'],
+	['sm', 'secondary', 'sm'],
+	['xs', 'warning', 'xs'],
+	['xxs', 'danger', 'xxs'],
+	['xxxs', 'danger', 'xxxs'],
+	['xxxxs', 'info', 'xxxxs'],
+	['xxxxxs', 'info', 'xxxxxs'],
+	undefined,
+	'lllllllllllllllloooooooooooooooonnnnnnnnnnnnnnnngggggggggggggggg');
